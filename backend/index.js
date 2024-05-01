f324c010511412c9a3afef240e4638aa70f5c6c5
yashkumar ram <yram@netclues.lan>
2024-05-01 20:44:23 +0530

    Chat-APP

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express()
+const http = require('http').createServer(app)
+
+var io = require('socket.io')(http);
+const cors = require('cors');
+// app.use(cors());
+const PORT = 3050;
+
+http.listen(PORT, () => {
+    console.log(`Server is started on port ${PORT}.`);
+})
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+
+let users = [];
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('message', (data) => {
+    socketIO.emit('messageResponse', data);
+  });
+
+  //Listens when a new user joins the server
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+    socket.disconnect();
+  });
+});
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello world',
+    });
+});
+
+
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+
+    console.log('new client connected');
+
+});
